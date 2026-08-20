3789f41cf51156f87b2d1daa3a38901ab2e42445
Somer Hanna <somerhanna10@gmail.com>
2026-08-20 10:32:13 -0700

    remove I2C speed

--- ChickenDoor/src/display.cpp
+++ ChickenDoor/src/display.cpp
@@ -24,7 +24,7 @@ bool oledReady = false;
 // =====================================================
 void initOLED() {
   Wire.begin(OLED_SDA, OLED_SCL);
-  Wire.setClock(400000); // 400kHz I2C speed
+  //Wire.setClock(400000); // 400kHz I2C speed
   display.begin();
   display.setContrast(255);
   oledReady = true;
